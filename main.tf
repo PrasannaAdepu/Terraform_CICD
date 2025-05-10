58e8e3e3d4e2854351dcad48e506b49729294a6d
PrasannaAdepu <prasanna7gp@gmail.com>
2025-05-11 05:27:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-00db8dadb36c9815e"
-    instance_type = "t2.nano"
+    ami = "ami-0f88e80871fd81e91"
+    instance_type = "t2.micro"
     tags = {
       Name = "test"
     }
